6e42387aa93799d7f43dee86efd2271a68220eb1
Fondue Port <rwrubio3@gmail.com>
2016-12-05 15:23:31 -0500

    Started working on the create-account component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
