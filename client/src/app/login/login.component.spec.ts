dff9dff28efe8c262bfd0970b93ee448a8cb8de7
Matthew Dickinson <matt@codifi.us>
2016-12-07 16:08:18 -0500

    Finished tests for login component

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,12 +1,14 @@
+import {Observable} from "rxjs";
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
-import { CustomUserApi } from '../shared/sdk/services/custom/CustomUser';
+import { CustomUserApi } from '../shared/sdk/services';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -16,9 +18,14 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       providers: [
-        {provide: CustomUserApi, useValue: {login: function(){}}}
+        {provide: CustomUserApi, useValue: {
+          login: jasmine.createSpy('login')
+        }},
+        {provide: Router, useValue: {
+          navigateByUrl: jasmine.createSpy('navigateByUrl')
+        }}
       ],
-      imports: [FormsModule, RouterTestingModule.withRoutes([]) ]
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
@@ -31,5 +38,42 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call login when form is submitted', () => {
+    fixture.detectChanges();
+    const form = fixture.debugElement.query(By.css('form'));
+    spyOn(component, 'login');
+
+    //button.nativeElement.click();
+    form.triggerEventHandler('submit', null);
+    fixture.detectChanges();
+    expect(component.login).toHaveBeenCalled();
+  });
+
+  it('should call login on click', () => {
+    const userApi = fixture.debugElement.injector.get(CustomUserApi);
+    const router = fixture.debugElement.injector.get(Router);
+
+    userApi.login.and.returnValue(Observable.of({id: 1, email: 't@t.si', firstname: 'test', lastname: 'thing'}));
+
+    expect(userApi.login).not.toHaveBeenCalled();
+    component.login();
+    expect(userApi.login).toHaveBeenCalled();
+    expect(component.loginError).toBeFalsy();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/');
+  });
+
+  it('should show error on failed login', () => {
+    const userApi = fixture.debugElement.injector.get(CustomUserApi);
+    const router = fixture.debugElement.injector.get(Router);
+
+    userApi.login.and.returnValue(Observable.throw('test error'));
+
+    expect(userApi.login).not.toHaveBeenCalled();
+    component.login();
+    expect(userApi.login).toHaveBeenCalled();
+    expect(component.loginError).toBe('Invalid Login');
+    expect(router.navigateByUrl).not.toHaveBeenCalled();
   });
 });
