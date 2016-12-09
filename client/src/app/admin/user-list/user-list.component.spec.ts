b44c993959e35a2d53f7fb3a2239eeea176d85dd
Matthew Dickinson <matt@codifi.us>
2016-12-08 22:25:59 -0500

    Added password reset and user delete to user-list

--- client/src/app/admin/user-list/user-list.component.spec.ts
+++ client/src/app/admin/user-list/user-list.component.spec.ts
@@ -24,7 +24,9 @@ describe('UserListComponent', () => {
 			declarations: [ UserListComponent ],
 			providers: [
 				{provide: CustomUserApi, useValue: {
-					find: jasmine.createSpy('find')
+					find: jasmine.createSpy('find'),
+					deleteById: jasmine.createSpy('deleteById'),
+					patchAttributes: jasmine.createSpy('patchAttributes'),
 				}}
 			],
 			imports: [FormsModule]
@@ -40,6 +42,36 @@ describe('UserListComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('show show success message for 5 seconds on showSuccessMessage("")', () => {
+		jasmine.clock().uninstall();
+		jasmine.clock().install();
+		expect(component.displaySuccess).toBeNull();
+		const message = "message";
+		component.showSuccessMessage(message);
+		expect(component.displaySuccess).toBe(message);
+		jasmine.clock().tick(4500);
+		expect(component.displaySuccess).toBe(message);
+		jasmine.clock().tick(501);
+		expect(component.displaySuccess).toBeNull();
+		jasmine.clock().uninstall();
+	});
+
+	it('should show success message when displaySuccess property is filled', () => {
+		spyOn(component, 'ngOnInit'); //skip init search
+		const message = "message";
+		component.displaySuccess = message;
+		fixture.detectChanges();
+
+		let messageDiv = fixture.debugElement.query(By.css('div.alert-success'));
+		expect(messageDiv).toBeTruthy();
+		expect(messageDiv.nativeElement.textContent.trim()).toBe(message);
+
+		component.displaySuccess = null;
+		fixture.detectChanges();
+		messageDiv = fixture.debugElement.query(By.css('div.alert-success'));
+		expect(messageDiv).toBeFalsy();
 	});
 
 	it('should lookup users on load', () => {
@@ -120,5 +152,115 @@ describe('UserListComponent', () => {
 		}});
 		expect(component.userList).toEqual(expectedUsers);
 		expect(component.displayError).toBeNull();
+	});
+
+	it('should call deleteUser on delete', () => {
+		spyOn(component, 'ngOnInit'); //skip init search
+
+		component.userList = expectedUsers;
+		fixture.detectChanges();
+		spyOn(component, 'deleteUser');
+
+		expect(component.deleteUser).not.toHaveBeenCalled();
+		const firstDelete = fixture.debugElement.query(By.css('table tbody tr td a[title="Delete"]'));
+		firstDelete.triggerEventHandler('click', null);
+		expect(component.deleteUser).toHaveBeenCalledWith(expectedUsers[0]);
+	});
+
+	it('should call CustomUserApi.deleteById on delete (confirm)', () => {
+		spyOn(window, 'confirm').and.returnValue(true);
+		component.userList = expectedUsers.slice();
+		const userApi = fixture.debugElement.injector.get(CustomUserApi);
+
+		expect(userApi.deleteById).not.toHaveBeenCalled();
+
+		userApi.deleteById.and.returnValue(Observable.of({}));
+
+		component.deleteUser(expectedUsers[0]);
+
+		expect(userApi.deleteById).toHaveBeenCalledWith(expectedUsers[0].id);
+		expect(component.userList.length).toEqual(1);
+		expect(component.userList[0].id).toBe(expectedUsers[1].id);
+		expect(component.displayError).toBeNull();
+	});
+
+	it('should call CustomUserApi.deleteById on delete and show message on error', () => {
+		spyOn(window, 'confirm').and.returnValue(true);
+		component.userList = expectedUsers.slice();
+		const userApi = fixture.debugElement.injector.get(CustomUserApi);
+
+		expect(userApi.deleteById).not.toHaveBeenCalled();
+
+		const expectedError = {message: 'failure'};
+		userApi.deleteById.and.returnValue(Observable.throw(expectedError));
+
+		component.deleteUser(expectedUsers[0]);
+
+		expect(userApi.deleteById).toHaveBeenCalledWith(expectedUsers[0].id);
+		expect(component.userList.length).toEqual(2);
+		expect(component.displayError).toBe(expectedError.message);
+	});
+
+	it('should not call CustomUserApi.deleteById on delete (cancelled)', () => {
+		spyOn(window, 'confirm').and.returnValue(false);
+		component.userList = expectedUsers.slice();
+		const userApi = fixture.debugElement.injector.get(CustomUserApi);
+
+		expect(userApi.deleteById).not.toHaveBeenCalled();
+
+		userApi.deleteById.and.returnValue(Observable.of({}));
+
+		component.deleteUser(expectedUsers[0]);
+
+		expect(userApi.deleteById).not.toHaveBeenCalled();
+		expect(component.userList.length).toEqual(2);
+		expect(component.displayError).toBeNull();
+	});
+
+
+	it('should call resetPassword on reset', () => {
+		spyOn(component, 'ngOnInit'); //skip init search
+
+		component.userList = expectedUsers;
+		fixture.detectChanges();
+		spyOn(component, 'resetPassword');
+
+		expect(component.resetPassword).not.toHaveBeenCalled();
+		const firstDelete = fixture.debugElement.query(By.css('table tbody tr td a[title="Reset Password"]'));
+		firstDelete.triggerEventHandler('click', null);
+		expect(component.resetPassword).toHaveBeenCalledWith(expectedUsers[0]);
+	});
+
+	it('should call CustomUserApi.patchAttributes on resetPassword (good password)', () => {
+		let successMessageFunc = spyOn(component, 'showSuccessMessage');
+		const expectedPassword = "val";
+		spyOn(component, 'getRandomPassword').and.returnValue(expectedPassword);
+
+		const userApi = fixture.debugElement.injector.get(CustomUserApi);
+
+		expect(userApi.patchAttributes).not.toHaveBeenCalled();
+
+		userApi.patchAttributes.and.returnValue(Observable.of({}));
+
+		component.resetPassword(expectedUsers[0]);
+
+		expect(userApi.patchAttributes).toHaveBeenCalledWith(expectedUsers[0].id, {password: expectedPassword});
+		expect(successMessageFunc).toHaveBeenCalledWith("Password reset successfully");
+		expect(component.displayError).toBeNull();
+	});
+
+	it('should call CustomUserApi.deleteById on delete and show message on error', () => {
+		spyOn(component, 'getRandomPassword').and.returnValue('sdf')
+
+		const userApi = fixture.debugElement.injector.get(CustomUserApi);
+
+		expect(userApi.patchAttributes).not.toHaveBeenCalled();
+
+		userApi.patchAttributes.and.returnValue(Observable.throw({message: 'failure'}));
+
+		component.resetPassword(expectedUsers[0]);
+
+		expect(userApi.patchAttributes).toHaveBeenCalled();
+		expect(component.displayError).toBe("Password reset failed");
 	});
 });
--- client/src/app/admin/user-list/user-list.component.ts
+++ client/src/app/admin/user-list/user-list.component.ts
@@ -10,6 +10,7 @@ import { CustomUser } from "../../shared/sdk/models";
 export class UserListComponent implements OnInit {
 	userList: CustomUser[];
 	displayError: string = null;
+	displaySuccess: string = null;
 	searchString = "";
 
 	constructor(private userApi: CustomUserApi) { }
@@ -29,13 +30,54 @@ export class UserListComponent implements OnInit {
 		}});
 	}
 
-	runSearch(filter?) {
+	deleteUser(user: CustomUser){
+		if (confirm(`Are you sure you want to delete ${user.firstName} ${user.lastName} (${user.id})`)){
+			this.userApi.deleteById(user.id).subscribe(() => {
+				this.displayError = null;
+				this.userList.splice(this.userList.findIndex(u => u.id == user.id), 1);
+			}, err => {
+				this.displayError = this.getErrorMessage(err);
+			});
+		}
+	}
+
+	resetPassword(user: CustomUser){
+		const newPassword = this.getRandomPassword(user);
+		if (newPassword) {
+			this.userApi.patchAttributes(user.id, {password: newPassword}).subscribe(() => {
+				this.showSuccessMessage("Password reset successfully");
+			}, err => {
+				this.displayError = "Password reset failed";
+			})
+		} else {
+			this.displayError = "Password reset cancelled";
+		}
+	}
+
+	showSuccessMessage(message: string){
+		this.displaySuccess = message;
+		setTimeout(() => {
+			this.displaySuccess = null;
+		}, 5000);
+	}
+
+	private getRandomPassword (user: CustomUser){
+		//return Math.random().toString(36).slice(2);
+		return prompt(`Enter a new password for ${user.firstName} ${user.lastName} (${user.id})`);
+	}
+
+	private runSearch(filter?) {
 		this.userApi.find(filter).subscribe((list: CustomUser[]) => {
 			this.userList = list;
+			this.displayError = null;
 		}, err => {
 			console.log('there was an error during searching', err);
-			this.displayError = (err instanceof Error || typeof err === "object" && err.message) ? err.message : err.toString();
+			this.displayError = this.getErrorMessage(err);
 		})
+	}
+
+	private getErrorMessage (err: any) {
+		return (err instanceof Error || typeof err === "object" && err.message) ? err.message : err.toString();
 	}
 
 }
