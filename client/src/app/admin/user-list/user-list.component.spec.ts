cba8417fb6337d1f259776026674e53b55c418cb
Matthew Dickinson <matt@codifi.us>
2016-12-14 10:57:32 -0500

    Added ability to promote and remove admins

--- client/src/app/admin/user-list/user-list.component.spec.ts
+++ client/src/app/admin/user-list/user-list.component.spec.ts
@@ -6,10 +6,11 @@ import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Observable } from "rxjs";
 
-import { CustomUser } from "../../shared/sdk/models";
-import { CustomUserApi } from '../../shared/sdk/services';
+import { CustomUser, Role } from "../../shared/sdk/models";
+import { CustomUserApi, RoleMappingApi, RoleApi } from '../../shared/sdk/services';
 import { UserListComponent } from './user-list.component';
 import { FlashMessageService } from '../../flash-message/flash-message.service';
+import { FlashMessageHelper } from '../../flash-message/flash-message.helper';
 
 describe('UserListComponent', () => {
 
@@ -20,7 +21,10 @@ describe('UserListComponent', () => {
 
 	let component: UserListComponent;
 	let fixture: ComponentFixture<UserListComponent>;
+	let flashHelper: FlashMessageHelper;
 	let userApi: any;
+	let roleApi: any;
+	let roleMappingApi: any;
 	let flashMessageService: any;
 
 	beforeEach(async(() => {
@@ -31,6 +35,14 @@ describe('UserListComponent', () => {
 					find: jasmine.createSpy('find'),
 					deleteById: jasmine.createSpy('deleteById'),
 					patchAttributes: jasmine.createSpy('patchAttributes'),
+				}},
+				{provide: RoleApi, useValue: {
+					findOne: jasmine.createSpy('findOne').and.returnValue(Observable.of({}))
+				}},
+				{provide: RoleMappingApi, useValue: {
+					create: jasmine.createSpy('create').and.returnValue(Observable.empty()),
+					deleteById: jasmine.createSpy('deleteById').and.returnValue(Observable.empty()),
+					findOne: jasmine.createSpy('findOne').and.returnValue(Observable.of({}))
 				}},
 				{provide: FlashMessageService, useValue: {
 					showMessage: jasmine.createSpy('showMessage')
@@ -46,7 +58,10 @@ describe('UserListComponent', () => {
 		component = fixture.componentInstance;
 		// fixture.detectChanges();
 		userApi = fixture.debugElement.injector.get(CustomUserApi);
+		roleMappingApi = fixture.debugElement.injector.get(RoleMappingApi);
+		roleApi = fixture.debugElement.injector.get(RoleApi);
 		flashMessageService = fixture.debugElement.injector.get(FlashMessageService);
+		flashHelper = new FlashMessageHelper(flashMessageService);
 	});
 
 	it('should create', () => {
@@ -55,16 +70,19 @@ describe('UserListComponent', () => {
 
 	it('should lookup users on load', () => {
 		expect(userApi.find).not.toHaveBeenCalled();
+		expect(roleApi.findOne).not.toHaveBeenCalled();
 
 		userApi.find.and.returnValue(Observable.of(expectedUsers));
 
 		fixture.detectChanges();
 		expect(userApi.find).toHaveBeenCalled();
 		expect(component.userList).toEqual(expectedUsers);
+		expect(roleApi.findOne).toHaveBeenCalled();
 	});
 
 	it('should show error when user lookup fails', () => {
 		expect(userApi.find).not.toHaveBeenCalled();
+		expect(roleApi.findOne).not.toHaveBeenCalled();
 		const expectedErr = "Test error"
 
 		userApi.find.and.returnValue(Observable.throw(new Error(expectedErr)));
@@ -72,7 +90,8 @@ describe('UserListComponent', () => {
 		fixture.detectChanges();
 		expect(userApi.find).toHaveBeenCalled();
 		expect(component.userList).toBeUndefined();
-		expect(flashMessageService.showMessage).toHaveBeenCalledWith({message: expectedErr, messageClass: 'danger'});
+		flashHelper.expectFailure(expectedErr);
+		expect(roleApi.findOne).toHaveBeenCalled();
 	});
 
 	it('should bind users to the frontend', () => {
@@ -119,7 +138,7 @@ describe('UserListComponent', () => {
 				{lastName: {like: searchPattern}},
 				{email: {like: searchPattern}}
 			]
-		}});
+		}, include: 'roles'});
 		expect(component.userList).toEqual(expectedUsers);
 		expect(component.displayError).toBeNull();
 	});
@@ -151,7 +170,7 @@ describe('UserListComponent', () => {
 		expect(component.userList.length).toEqual(1);
 		expect(component.userList[0].id).toBe(expectedUsers[1].id);
 		expect(component.displayError).toBeNull();
-		expect(flashMessageService.showMessage).toHaveBeenCalledWith({message: 'Deleted User', messageClass: 'success'});
+		flashHelper.expectSuccess('Deleted User');
 	});
 
 	it('should call CustomUserApi.deleteById on delete and show message on error', () => {
@@ -167,7 +186,7 @@ describe('UserListComponent', () => {
 
 		expect(userApi.deleteById).toHaveBeenCalledWith(expectedUsers[0].id);
 		expect(component.userList.length).toEqual(2);
-		expect(flashMessageService.showMessage).toHaveBeenCalledWith({message: expectedError.message, messageClass: 'danger'})
+		flashHelper.expectFailure(expectedError.message);
 	});
 
 	it('should not call CustomUserApi.deleteById on delete (cancelled)', () => {
@@ -209,7 +228,7 @@ describe('UserListComponent', () => {
 		component.resetPassword(expectedUsers[0]);
 
 		expect(userApi.patchAttributes).toHaveBeenCalledWith(expectedUsers[0].id, {password: expectedPassword});
-		expect(flashMessageService.showMessage).toHaveBeenCalledWith({message: "Password reset successfully", messageClass: 'success'})
+		flashHelper.expectSuccess("Password reset successfully");
 	});
 
 	it('should call CustomUserApi.deleteById on delete and show message on error', () => {
@@ -222,6 +241,125 @@ describe('UserListComponent', () => {
 		component.resetPassword(expectedUsers[0]);
 
 		expect(userApi.patchAttributes).toHaveBeenCalled();
-		expect(flashMessageService.showMessage).toHaveBeenCalledWith({message: "Password reset failed", messageClass: 'danger'})
+		flashHelper.expectFailure("Password reset failed");
+	});
+
+	it('should check roles for userIsAdmin', () => {
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+
+		const user = new CustomUser({id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b'});
+
+		//expect no roles to be false
+		expect(component.userIsAdmin(user)).toBeFalsy();
+
+		//expect empty roles to be false
+		user.roles = [];
+		expect(component.userIsAdmin(user)).toBeFalsy();
+
+		//expect wrong roles to be false
+		user.roles = [new Role({id: 2, name: 'face'})];
+		expect(component.userIsAdmin(user)).toBeFalsy();
+
+		user.roles.push(new Role({id: 99, name: 'admin'}));
+		expect(component.userIsAdmin(user)).toBeTruthy();
+	});
+
+	it('should properly remove admin in toggleAdminStatus', () => {
+		spyOn(component, 'userIsAdmin').and.returnValue(true);
+
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+
+		const matchingRoleMap = {id: 42}
+		roleMappingApi.findOne.and.returnValue(Observable.of(matchingRoleMap));
+		roleMappingApi.deleteById.and.returnValue(Observable.of({id: 1}));
+
+		const user = new CustomUser({
+			id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b',
+			roles: [new Role({id: 99, name: 'admin'})]
+		});
+
+		component.toggleAdminStatus(user);
+
+		expect(roleMappingApi.findOne).toHaveBeenCalledWith({
+			where: {principalId: user.id, principalType: 'USER', roleId: component.adminRole.id}
+		});
+		expect(roleMappingApi.deleteById).toHaveBeenCalledWith(matchingRoleMap.id);
+		flashHelper.expectSuccess(`${user.firstName} ${user.lastName} (${user.id}) is no longer admin`);
+		expect(user.roles.length).toBe(0);
+	});
+
+	it('should handle failure on toggleAdminStatus error (find)', () => {
+		spyOn(component, 'userIsAdmin').and.returnValue(true);
+
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+
+		roleMappingApi.findOne.and.returnValue(Observable.throw("error!!!"));
+		roleMappingApi.deleteById.and.returnValue(Observable.of({id: 1}));
+
+		const user = new CustomUser({
+			id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b',
+			roles: [new Role({id: 99, name: 'admin'})]
+		});
+
+		component.toggleAdminStatus(user);
+
+		expect(roleMappingApi.findOne).toHaveBeenCalled();
+		expect(roleMappingApi.deleteById).not.toHaveBeenCalled();
+		flashHelper.expectFailure('Unable to remove admin');
+		expect(user.roles.length).toBe(1);
+	});
+
+	it('should handle failure on toggleAdminStatus error (delete)', () => {
+		spyOn(component, 'userIsAdmin').and.returnValue(true);
+
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+
+		const matchingRoleMap = {id: 42}
+		roleMappingApi.findOne.and.returnValue(Observable.of(matchingRoleMap));
+		roleMappingApi.deleteById.and.returnValue(Observable.throw("error!!!!"));
+
+		const user = new CustomUser({
+			id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b',
+			roles: [new Role({id: 99, name: 'admin'})]
+		});
+
+		component.toggleAdminStatus(user);
+
+		expect(roleMappingApi.findOne).toHaveBeenCalled();
+		expect(roleMappingApi.deleteById).toHaveBeenCalled();
+		flashHelper.expectFailure('Unable to remove admin');
+		expect(user.roles.length).toBe(1);
+	});
+
+	it('should properly add an admin in toggleAdminStatus', () => {
+		spyOn(component, 'userIsAdmin').and.returnValue(false);
+
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+		const user = new CustomUser({ id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b' });
+
+		roleMappingApi.create.and.returnValue(Observable.of(component.adminRole));
+
+		component.toggleAdminStatus(user);
+
+		expect(roleMappingApi.create).toHaveBeenCalledWith({
+			principalId: user.id, principalType: 'USER', roleId: component.adminRole.id
+		});
+		expect(user.roles).toEqual([component.adminRole]);
+		flashHelper.expectSuccess(`${user.firstName} ${user.lastName} (${user.id}) is now admin`);
+	});
+
+	it('should properly add an admin in toggleAdminStatus', () => {
+		spyOn(component, 'userIsAdmin').and.returnValue(false);
+
+		component.adminRole = new Role({id: 99, name: 'adminTest'});
+		const user = new CustomUser({ id: 1, firstName: '1', lastName: '2', email: 'a', password: 'b' });
+
+		roleMappingApi.create.and.returnValue(Observable.throw('error'));
+
+		component.toggleAdminStatus(user);
+
+		expect(roleMappingApi.create).toHaveBeenCalled();
+		expect(user.roles).toBeUndefined();
+		flashHelper.expectFailure('Unable to create admin');
 	});
 });
--- client/src/app/admin/user-list/user-list.component.ts
+++ client/src/app/admin/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import {FlashMessageService} from "../../flash-message/flash-message.service";
 import { Component, OnInit } from '@angular/core';
-import { CustomUserApi } from '../../shared/sdk/services';
-import { CustomUser } from "../../shared/sdk/models";
+import { CustomUserApi, RoleMappingApi, RoleApi } from '../../shared/sdk/services';
+import { CustomUser, LoopBackFilter, Role, RoleMapping } from "../../shared/sdk/models";
 
 @Component({
 	selector: 'app-user-list',
@@ -9,26 +9,29 @@ import { CustomUser } from "../../shared/sdk/models";
 	styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+	adminRole: Role;
 	userList: CustomUser[];
 	displayError: string = null;
 	displaySuccess: string = null;
 	searchString = "";
 
-	constructor(private userApi: CustomUserApi, private flashMessageService: FlashMessageService) { }
+	constructor(private userApi: CustomUserApi, private roleMappingApi: RoleMappingApi, private roleApi: RoleApi, private flashMessageService: FlashMessageService) { }
 
 	ngOnInit() {
 		this.runSearch();
+
+		this.roleApi.findOne({where: {name: 'admin'}}).subscribe((r: Role) => this.adminRole = r);
 	}
 
 	search() {
 		const searchPattern = `%${this.searchString}%`;
-		this.runSearch({where: {
+		this.runSearch({
 			or: [
 				{firstName: {like: searchPattern}},
 				{lastName: {like: searchPattern}},
 				{email: {like: searchPattern}}
 			]
-		}});
+		});
 	}
 
 	deleteUser(user: CustomUser){
@@ -56,12 +59,58 @@ export class UserListComponent implements OnInit {
 		}
 	}
 
+	userIsAdmin(user: CustomUser){
+		return user.roles && user.roles.find(r => r.id == this.adminRole.id) != null;
+	}
+
+	toggleAdminStatus(user: CustomUser) {
+		//if this user is already an admin, create the role mapping
+		if (this.userIsAdmin(user)){
+			//find which rolemapping is attached to this user and role
+			this.roleMappingApi.findOne({
+				where: {principalId: user.id, principalType: 'USER', roleId: this.adminRole.id}
+			}).flatMap((rm: RoleMapping) => {
+				//if we round the matching role, remove it
+				return this.roleMappingApi.deleteById(rm.id);
+			}).subscribe(res => {
+				//once removed, remove it from the user object
+				const roleToRemoveIdx = user.roles.findIndex(r => r.id == this.adminRole.id);
+				user.roles.splice(roleToRemoveIdx, 1);
+				this.flashMessageService.showMessage({
+					message: `${user.firstName} ${user.lastName} (${user.id}) is no longer admin`,
+					messageClass: 'success'
+				});
+			}, err => {
+				this.flashMessageService.showMessage({message: "Unable to remove admin", messageClass: 'danger'});
+			});
+		} else { //if the user is not currently an admin
+			//create the new role mapping
+			this.roleMappingApi.create({
+				principalId: user.id, principalType: 'USER', roleId: this.adminRole.id
+			}).subscribe(rm => {
+				//when the map finishes, add it to the user list
+				if (!user.roles) user.roles = [];
+				user.roles.push(this.adminRole);
+				this.flashMessageService.showMessage({
+					message: `${user.firstName} ${user.lastName} (${user.id}) is now admin`,
+					messageClass: 'success'
+				});
+			}, err => {
+				this.flashMessageService.showMessage({message: "Unable to create admin", messageClass: 'danger'});
+			})
+		}
+	}
+
 	private getRandomPassword (user: CustomUser){
 		//return Math.random().toString(36).slice(2);
 		return prompt(`Enter a new password for ${user.firstName} ${user.lastName} (${user.id})`);
 	}
 
-	private runSearch(filter?) {
+	private runSearch(whereClause?) {
+		const filter: LoopBackFilter = {include: 'roles'};
+		if (whereClause) {
+			filter.where = whereClause;
+		}
 		this.userApi.find(filter).subscribe((list: CustomUser[]) => {
 			this.userList = list;
 		}, err => {
