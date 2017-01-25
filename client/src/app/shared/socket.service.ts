cc1c0e8d7822f09a5c4e3b7b297607dc10f2353f
Matthew Dickinson <matt@codifi.us>
2017-01-25 14:13:40 -0500

    Added test for socket service

--- client/src/app/shared/socket.service.ts
+++ client/src/app/shared/socket.service.ts
@@ -20,36 +20,19 @@ export class SocketService {
 	}>();
 
 
-	private socket:SocketIOClient.Socket;
+	private socket: SocketIOClient.Socket;
 
 	constructor() {
-		this.socket = io(LoopBackConfig.getPath());
-		this.socket.on('connect', (socket: SocketIOClient.Emitter) => {
-			console.log('connected!!!');
-		});
+		//this.socket = io(LoopBackConfig.getPath());
+		this.socket = io.connect(LoopBackConfig.getPath());
+		// this.socket.on('connect', (socket: SocketIOClient.Emitter) => {
+		// 	console.log('connected!!!');
+		// });
 	}
 
 	watchEvent (eventName: string): Observable<SocketMessage> {
-		console.log("request to watch", eventName, this.eventMap.get(eventName));
-		let subject;
-
-		if (this.eventMap.has(eventName)){
-			//if this event exists in our listener cache, grab it and update the reference count
-			const subscr = this.eventMap.get(eventName);
-			subscr.count = subscr.count + 1;
-			this.eventMap.set(eventName, subscr);
-
-			subject = subscr.subject;
-		} else {
-			//if we don't have a listener cached, create a new subject and push it in cache
-			subject = new Subject<SocketMessage>();
-			this.eventMap.set(eventName, {count: 1, subject: subject});
-
-			//tell socket listen for this event and publish on our new subject
-			this.socket.on(eventName, (item: any) => {
-				subject.next({ event: eventName, item: item });
-			});
-		}
+		//console.log("request to watch", eventName, this.eventMap.get(eventName));
+		let subject = this.getSubjectForEvent(eventName);
 
 		// Return observable which follows the events on our subject (and updates reference counts on close)
 		return Observable.create((observer: any) => {
@@ -60,7 +43,7 @@ export class SocketService {
 			return () => {
 				if (this.eventMap.has(eventName)) {
 					const eventSubscription = this.eventMap.get(eventName);
-					eventSubscription.count = eventSubscription.count -1;
+					eventSubscription.count--;
 
 					//if all listeners for this event have been closed, get rid of the listener
 					if (eventSubscription.count == 0) {
@@ -75,19 +58,44 @@ export class SocketService {
 		});
 	}
 
-	joinChannel (roomName: string) {
-		//if we are already listening to this channel, ignore this request
-		if (this.channelList.indexOf(roomName) > -1) return;
+	private getSubjectForEvent(eventName: string): Subject<SocketMessage> {
 
-		this.channelList.push(roomName);
-		this.socket.emit('join-channel', roomName);
+		let subject: Subject<SocketMessage>;
+
+		if (this.eventMap.has(eventName)){
+			//if this event exists in our listener cache, grab it and update the reference count
+			const subscr = this.eventMap.get(eventName);
+			subscr.count++;
+			this.eventMap.set(eventName, subscr);
+
+			subject = subscr.subject;
+		} else {
+			//if we don't have a listener cached, create a new subject and push it in cache
+			subject = new Subject<SocketMessage>();
+			this.eventMap.set(eventName, {count: 1, subject: subject});
+
+			//tell socket listen for this event and publish on our new subject
+			this.socket.on(eventName, (item: any) => {
+				subject.next({ event: eventName, item: item });
+			});
+		}
+
+		return subject;
 	}
 
-	leaveChannel (roomName: string) {
-		//if we aren't listening to this channel, ignore this request
-		if (this.channelList.indexOf(roomName) == -1) return;
+	joinChannel (channelName: string) {
+		//if we are already listening to this channel, ignore this request
+		if (this.channelList.indexOf(channelName) > -1) return;
 
-		this.channelList.splice(this.channelList.indexOf(roomName), 1);
-		this.socket.emit('leave-channel', roomName);
+		this.channelList.push(channelName);
+		this.socket.emit('join-channel', channelName);
+	}
+
+	leaveChannel (channelName: string) {
+		//if we aren't listening to this channel, ignore this request
+		if (this.channelList.indexOf(channelName) == -1) return;
+
+		this.channelList.splice(this.channelList.indexOf(channelName), 1);
+		this.socket.emit('leave-channel', channelName);
 	}
 }
